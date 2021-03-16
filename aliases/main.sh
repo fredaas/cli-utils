87b722a64e0b3098644cf6d114a28f82482d5333
Fredrik Ås <fredaas86@gmail.com>
2021-03-16 19:31:51 +0100

    Change directory name
    Add alias for opening PDFs.

--- aliases/main.sh
+++ aliases/main.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+open-pdf()
+{
+    evince $1 &>/dev/null &
+}
+
 alias git-list-tracked="git ls-tree -r master --name-only"
 alias print-path="tr ':' '\n' <<< $PATH"
 alias print-man="man -k . s"
+alias pdfopen="open-pdf"
