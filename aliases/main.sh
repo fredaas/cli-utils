6a398de208481b4a59aab688e7a8f638d81e16ae
Fredrik Ås <>
2021-08-07 20:22:03 +0200

    Remove aliases

--- aliases/main.sh
+++
@@ -1,9 +0,0 @@
-macro::pdfopen()
-{
-    evince $1 &>/dev/null &
-}
-
-alias git-list-tracked="git ls-tree -r HEAD --name-only"
-alias print-path="tr ':' '\n' <<< $PATH"
-alias print-man="man -k . s"
-alias pdfopen=macro:pdfopen
