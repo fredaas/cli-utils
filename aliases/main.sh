e7876db936786b21ceef61d3bc47721d342ad3c2
Fredrik Ås <>
2021-08-07 19:42:01 +0200

    Update aliases

--- aliases/main.sh
+++ aliases/main.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/env bash
-
-open-pdf()
+macro::pdfopen()
 {
     evince $1 &>/dev/null &
 }
@@ -8,4 +6,4 @@ open-pdf()
 alias git-list-tracked="git ls-tree -r HEAD --name-only"
 alias print-path="tr ':' '\n' <<< $PATH"
 alias print-man="man -k . s"
-alias pdfopen="open-pdf"
+alias pdfopen=macro:pdfopen
